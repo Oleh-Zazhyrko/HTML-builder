b27e30483876062eccce7dd9bb918a8626592ebc
Oleh Zazhyrko <kosovo1989@gmail.com>
2022-11-05 20:42:00 +0100

    feat: add fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const fsProm = fs.promises;
+const path = require('path');
+
+const source = path.join(__dirname + '/files');
+const target = path.join(__dirname + '/files-copy');
+
+fsProm.mkdir(target, { recursive: false }).then(() => {
+  console.log(`Папка 'files-copy' была создана`);
+}).catch(() => {
+  console.log('Папка уже существует');
+});
+
+fsProm.readdir(source)
+  .then(files => {
+    for (let file of files) {
+      fsProm.copyFile(source + '/' + file, target + '/' + file);
+      console.log(`${file} скопирован успешно`);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  })
+ 
+
+
 
