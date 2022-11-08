ead1a88eae4d2c2156a89fce86e76a19ea8621bc
Oleh Zazhyrko <kosovo1989@gmail.com>
2022-11-08 17:25:45 +0100

    fix: third task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-const { Stream } = require('stream');
 const { stdin, stdout } = process;
 const output = fs.createWriteStream('02-write-file/text.txt');
+var tty = require("tty");
 
 stdout.write('Введите Ваш текст...\n');
 
@@ -12,9 +12,13 @@ stdin.on('data', (data, chunk) => {
     
     stdout.write('Удачи!');
     process.exit();
-  } else {    
+  } else if (typeof(text) === 'string' && text.indexOf('exit') === -1){
     chunk = data;
     output.write(chunk);
     stdout.write('Введите Ваш текст...\n');
+  } else {
+    stdout.write('Удачи!');
+    process.exit();
   }
 });
+
--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,15 +2,18 @@ const path = require('path');
 const fs = require('fs');
 
 const way = path.join(__dirname + '/secret-folder');
-console.log(`the way is ${way}`);
 
-let files = fs.readdirSync(way);
-
-files.forEach(file => {
-  fs.stat(`${way}` + '/' + `${file}`, (er, result) => {
-    if (er) throw er;
-    if (result.isFile()) {
-      console.log(file.split('.')[0] + ' - ' + file.split('.')[1] + ' - ' + (result.size/1024).toFixed(3) + 'kb');
-    }
-  })
-});
+fs.readdir(way, (err, files) => {
+  if (err)
+    console.log(err);
+  else {
+    files.forEach(file => {
+      fs.stat(`${way}` + '/' + `${file}`, (er, result) => {
+        if (er) throw er;
+        if (result.isFile()) {
+          console.log(file.split('.')[0] + ' - ' + file.split('.')[1] + ' - ' + (result.size/1024).toFixed(3) + 'kb');
+        }
+      })
+    });
+  }
+})
